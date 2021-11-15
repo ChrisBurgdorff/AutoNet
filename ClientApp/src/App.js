fec92fdd1dff91f394e728b548eb91a6b981d3da
Christopher Burgdorff <cburgdorff@pringle-quinn.com>
2021-11-15 05:03:14 -0500

    Updated UI

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -17,7 +17,7 @@ export default class App extends Component {
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
-            <Route path='/mint' component={Mint} />
+        <Route path='/mint' component={Mint} />
       </Layout>
     );
   }
--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace NFTDemo.Controllers
 {
@@ -11,6 +13,10 @@ namespace NFTDemo.Controllers
     [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+
+        //string devConnString = "Data Source=BEL-G09S-DEV;Initial Catalog=TAG;User ID=sa;Password=NCVMAag+^8ZrgGgt^jpdwB8rQZ5%eF;Integrated Security=False";
+        string stagingConnString = ConfigurationManager.ConnectionStrings["AzureSqlConnString"].ConnectionString;
+
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -26,12 +32,33 @@ namespace NFTDemo.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            string queryString = "SELECT Full_Name, Email FROM USERS";
+            List<String> lastNames = new List<string>();
+            using (SqlConnection conn = new SqlConnection(stagingConnString))
+            {
+                SqlCommand command = new SqlCommand(queryString, conn);
+                conn.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    try
+                    {
+                        while (reader.Read())
+                        {
+                            lastNames.Add(reader["Full_Name"].ToString());
+                        }
+                    }
+                    finally
+                    {
+                        reader.Close();
+                    }
+                }
+            }
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 2).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                Summary = lastNames[index]
             })
             .ToArray();
         }
